ad9d665b8cf1d7f369ed3f97caade256a1c87c83
Mykola Sirko <ssrko98@gmail.com>
2020-08-12 12:43:29 +0300

    Updates

---
+++ precache-manifest.9f11656c52d27a1a914b57f9644f644a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f2f0a88da78f3551c1ec8b227118e6",
+    "url": "/test-task-react/index.html"
+  },
+  {
+    "revision": "1bdf2a5b716e36c3cc4b",
+    "url": "/test-task-react/static/css/main.25d8153f.chunk.css"
+  },
+  {
+    "revision": "e9b2eb0ec90f31acd02b",
+    "url": "/test-task-react/static/js/2.69eaac58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-task-react/static/js/2.69eaac58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bdf2a5b716e36c3cc4b",
+    "url": "/test-task-react/static/js/main.e7ee030d.chunk.js"
+  },
+  {
+    "revision": "e52d84a04920ddfa6eb9",
+    "url": "/test-task-react/static/js/runtime-main.5b5a6e82.js"
+  },
+  {
+    "revision": "c888326ff7e5fc4ca32a525e0820a845",
+    "url": "/test-task-react/static/media/Avenir-Light.c888326f.ttf"
+  }
+]);
